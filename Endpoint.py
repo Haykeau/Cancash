345c57dc5669b7a7b3229d1cf86fd351cdd44f78
AntoineLBT <antoine.lebout@gmail.com>
2017-02-23 21:02:07 +0100

    dfz

--- Endpoint.py
+++ Endpoint.py
@@ -1,19 +1,19 @@
 class Endpoint():
-    
+
 
 
     def __init__(self, nbRequete,tpsCache, tpsDataCenter):
         self.nbRequete = nbRequete
         self.tpsCache = tpsCache
         self.tpsDataCenter = tpsDataCenter
-        self.numEndPoint = numEndPoint
+
 
     def chooseCache(tpsCache):
         mini = min(tpsCache)
         cacheChoisi = tpsCache.index(mini)
         return cacheChoisi
 
-    def tpsGagne(nbRequete,tpsCache,tpsDataCenter):
+    def tpsGagne(tpsCache,tpsDataCenter):
         cacheChoisi = chooseCache(tpsCache)
-        moyenne = 0
-        for video in nbRequete:
+        temps = tpsDataCenter - tpsCache[cacheChoisi]
+        return temps
