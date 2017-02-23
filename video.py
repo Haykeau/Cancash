17cf05f8bf827664188ef932115d6b5d62c14032
Haykeau <heko.chat@gmail.com>
2017-02-23 20:18:23 +0100

    update

--- video.py
+++ video.py
@@ -4,3 +4,6 @@ class Video:
     def __init__(self, numero, taille):
         self.numero = numero
         self.taille = taille
+
+    def getInformation(self):
+        return "There are " + self.numero + " videos"
