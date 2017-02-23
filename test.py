872fe6547221006421c770de1dfc053617cf1595
AntoineLBT <antoine.lebout@gmail.com>
2017-02-23 16:51:57 +0100

    reo

--- test.py
+++ test.py
@@ -7,10 +7,17 @@ babas = "gay"
 
 def calculerTonAge():
     ageInf1 = random.random()
-    print("Tu as :")
-    age = int(ageInf1*100)
-    print(age)
-    print("ans")
 
-while(True)
-    calculerTonAge()
+    age = int(ageInf1*100)
+    print("Tu as"+str(age)+"ans")
+    return age
+
+compteur = 0
+
+while(True):
+    age = calculerTonAge()
+    compteur+=1
+    if age == 99:
+        print("T'es vieux gros")
+        print(str(compteur)+"essai(s)")
+        break
