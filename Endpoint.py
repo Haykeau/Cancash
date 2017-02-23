7d442750b7b0c1f42acd0fd17b27223300a65889
AntoineLBT <antoine.lebout@gmail.com>
2017-02-23 20:28:14 +0100

    d

--- Endpoint.py
+++ Endpoint.py
@@ -2,7 +2,11 @@ class Endpoint():
     """docstring for Endpoint."""
 
 
-    def __init__(self, nbRequete[],tpsCache[], tpsDataCenter):
+    def __init__(self, nbRequete,tpsCache, tpsDataCenter):
         self.nbRequete = nbRequete
         self.tpsCache = tpsCache
         self.tpsDataCenter = tpsDataCenter
+
+    tpsGagne
+    def chooseCache(tpsCache)
+        cacheChoisi = min(tpsCache)
