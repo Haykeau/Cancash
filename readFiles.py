c3ff1a62c48777fdba79d2096e3398bbd136f39e
Haykeau <heko.chat@gmail.com>
2017-02-23 20:10:27 +0100

    update

--- readFiles.py
+++ readFiles.py
@@ -1,8 +1,18 @@
+import Endpoint
+import video
+import random
+
 with open("me_at_the_zoo.in","r") as hashTest :
     content = hashTest.readlines()
 content = [x.strip() for x in content]
 
 getInfo = []
+endpointList = []
+size = []
+
+for i in range(0,100) :
+    size.append(int(random.random()*10))
+
 
 array = content[0].split(" ")
 for char in range(0,len(array)):
@@ -13,5 +23,9 @@ numberEndpoint = getInfo[1]
 numberRequestDescription = getInfo[2]
 numberCache = getInfo[3]
 sizeCache = getInfo[4]
+
+video = Video(numberVideo,size)
+
+
 
 print getInfo
