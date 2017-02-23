8b74fd24abc7a56075d39f343bca7ed45d4c3bd0
Extro96 <extro96@gmail.com>
2017-02-23 21:16:24 +0100

    Update 21:16

--- submission.py
+++ submission.py
@@ -1,3 +1,6 @@
+from readFiles import *
+from cacheServer import *
+
 class submission:
 
     def __init__(self,nmbCacheServer, videosContenu):
@@ -5,14 +8,14 @@ class submission:
         self.videosContenu = videosContenu
 
         fichier = open("submission.txt", "w")
-        fichier.write(numberCache)
+        fichier.write(nmbCacheServer)
         fichier.write("/n")
         i = 0
         y = 0
-        while ( i <= numberCache):
-            while(y <= CacheServer.nombreVideo):
-                fichier.write("CacheServer.videoAGarder[i] ")
-                y += 1
+        while ( i <= nmbCacheServer):
+            fichier.write(i + " ")
+            for y in CacheServer.viderAGarder:
+                fichier.write(y)
             fichier.write("/n")
             i +=1
         fichier.close()
