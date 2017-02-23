e19393ea927dae9441fd91c1551480cbbe382270
AntoineLBT <antoine.lebout@gmail.com>
2017-02-23 20:44:33 +0100

    bite

--- Endpoint.py
+++ Endpoint.py
@@ -1,12 +1,20 @@
 class Endpoint():
-    """docstring for Endpoint."""
+    //docstring for Endpoint.
 
 
     def __init__(self, nbRequete,tpsCache, tpsDataCenter):
         self.nbRequete = nbRequete
         self.tpsCache = tpsCache
         self.tpsDataCenter = tpsDataCenter
+        self.numEndPoint = numEndPoint
 
-    tpsGagne
-    def chooseCache(tpsCache)
-        cacheChoisi = min(tpsCache)
+    def chooseCache(tpsCache):
+        mini = min(tpsCache)
+        cacheChoisi = tpsCache.index(mini)
+        return cacheChoisi
+
+    def tpsGagne(nbRequete,tpsCache,tpsDataCenter):
+        cacheChoisi = chooseCache(tpsCache)
+        moyenne = 0
+        for video in nbRequete:
+            
