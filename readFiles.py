f49e037b2c61dd4340ae81ff5185090b889f9516
Haykeau <heko.chat@gmail.com>
2017-02-23 21:30:45 +0100

    Update

--- readFiles.py
+++ readFiles.py
@@ -1,21 +1,34 @@
 from video import Video
+from Endpoint import Endpoint
 import random
 
-getInfo = []
-endpointList = []
+global content
 
-def cuteLine(content, nbLine):
+def cutLine(nbLine):
     array, cutedLine = [],[]
     array = content[nbLine].split(" ") #Split everytime the character " " appears
     for char in range(0,len(array)):
         cutedLine.append(array[char]) #Append each size in the getInfo parameter
     return cutedLine
 
+def createList(nbLine):
+    endList = []
+    pingList = []
+    theseLines = cutLine(nbLine)
+    nbItterations = theseLines[1]
+    itsPing = theseLines[0]
+    for i in range(0,int(theseLines[1])):
+        numberLine = nbLine+i
+        itte = cutLine(numberLine)
+        endList[i] = itte[0]
+        pingList[i] = itte[1]
+    return endList,pingList
+
 with open("me_at_the_zoo.in","r") as hashTest :
     content = hashTest.readlines()
 content = [x.strip() for x in content]
 
-getInfoTest = cuteLine(content,0)
+getInfo = cutLine(0)
 
 numberVideo = getInfo[0]
 numberEndpoint = getInfo[1]
@@ -25,4 +38,6 @@ sizeCache = getInfo[4]
 
 
 video = Video(getInfo[0],content[1]) #Video(number of video, size of each video)
-print video.getInformation()
+a,b = createList(2)
+print a
+print b
